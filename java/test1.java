c7bd0de0ec467b24529215d7ac8585005f114078
isikveren <lauxunzi@outlook.com>
2021-08-13 16:14:24 +0800

    update eva

---
+++ java/test1.java
@@ -0,0 +1,8 @@
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        //test1 test1 = new test1();
+        test2.IsSon();
+    }
+}
---
+++ java/test2.java
@@ -0,0 +1,5 @@
+public class test2 {
+    public static void IsSon(){
+        System.out.println("static IsSon is OK");
+    }
+}
